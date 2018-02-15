57ff1a9cd040b0c7fa8b3ed206ccfe594b0a3af6
Marcin Pastwa <marcinpastwa@wp.pl>
2018-02-15 21:59:40 +0100

    test done

--- google-client/src/test/java/com/infoshareacademy/api/states/StateServiceTest.java
+++ google-client/src/test/java/com/infoshareacademy/api/states/StateServiceTest.java
@@ -5,13 +5,19 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import java.util.List;
 
 public class StateServiceTest {
 
     private StateService stateService;
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
     @Before
     public void setUp(){
          stateService = new StateService();
@@ -28,6 +34,13 @@ public class StateServiceTest {
         List<StateDetails> stateDetailsList = stateService.getAllStates();
         int numberOfState = stateDetailsList.size();
         assertThat(numberOfState,is(55));
+    }
+
+    @Test
+    public void shouldHandleFailure() {
+        expectedException.expect(NullPointerException.class);
+//        expectedException.expectMessage();
+        stateService.getStateNameByAbbr("WAA");
     }
 
 }
